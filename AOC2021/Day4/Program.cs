f0d7809a1209dba4383316cd0a715a52a7664c5e
Kamichalik1 <kacper.m.michalik@gmail.com>
2021-12-04 13:57:33 +0000

    Day 4 Part 2

--- AOC2021/Day4/Program.cs
+++ AOC2021/Day4/Program.cs
@@ -142,8 +142,67 @@ namespace Day4
         {
             string[] Lines = System.IO.File.ReadAllLines("Data.txt");
 
+            string[] DrawnStrings = Lines[0].Split(',');
+            List<int> DrawnValues = new List<int>();
 
-            Console.WriteLine();
+            for (int i = 0; i < DrawnStrings.Length; i++)
+            {
+                DrawnValues.Add(Convert.ToInt32(DrawnStrings[i]));
+            }
+
+            List<Board> Boards = new List<Board>();
+
+            int BoardSize = 5;
+
+            for (int i = 2; i < Lines.Length; i += BoardSize + 1)
+            {
+                Board CurrentBoard = new Board(new int[BoardSize, BoardSize], new bool[BoardSize, BoardSize]);
+                for (int y = 0; y < BoardSize; y++)
+                {
+                    string[] Values = Lines[i + y].Split(' ');
+                    int x = 0;
+
+                    for (int k = 0; k < Values.Length; k++)
+                    {
+                        if (Values[k] != "")
+                        {
+                            CurrentBoard.Values[x, y] = Convert.ToInt32(Values[k]);
+                            CurrentBoard.IsGuessed[x, y] = false;
+                            x++;
+                        }
+                    }
+                }
+                Boards.Add(CurrentBoard);
+
+            }
+
+            int LastWinningScore = 0;
+
+            for (int i = 0; i < DrawnValues.Count; i++)
+            {
+                for (int j = 0; j < Boards.Count; j++)
+                {
+                    Boards[j].Guess(DrawnValues[i]);
+                    if (Boards[j].IsBoardComplete())
+                    {
+                        if (Boards.Count == 1)
+                        {
+                            LastWinningScore = Boards[j].Score(DrawnValues[i]);
+                            i = DrawnValues.Count;
+                            j = Boards.Count;
+                        }
+                        else
+                        {
+                            Boards.RemoveAt(j);
+                            j--;
+                        }
+                    }
+
+                }
+            }
+
+
+            Console.WriteLine(LastWinningScore);
             Console.ReadKey();
         }
     }
