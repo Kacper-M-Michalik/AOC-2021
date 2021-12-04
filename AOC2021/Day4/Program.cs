b33fc3686bc6d168154c4eb51f6108a14f7c7f28
Kamichalik1 <kacper.m.michalik@gmail.com>
2021-12-04 13:47:22 +0000

    Day 4 Part 1

---
+++ AOC2021/Day4/Program.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        public class Board
+        {
+            public int[,] Values;
+            public bool[,] IsGuessed;
+
+            public Board(int[,] NewValues, bool[,] NewIsGuessed)
+            {
+                Values = NewValues;
+                IsGuessed = NewIsGuessed;
+            }
+
+            public void Guess(int Number)
+            {
+                for (int y = 0; y < IsGuessed.GetLength(1); y++)
+                {
+                    for (int x = 0; x < IsGuessed.GetLength(0); x++)
+                    {
+                        if (Values[x, y] == Number) IsGuessed[x,y] = true;
+                    }
+                }
+            }
+
+            public int Score(int Multiplier)
+            {
+                int Sum = 0;
+                for (int y = 0; y < IsGuessed.GetLength(1); y++)
+                {
+                    for (int x = 0; x < IsGuessed.GetLength(0); x++)
+                    {
+                        if (IsGuessed[x, y] == false) Sum += Values[x, y];
+                    }
+                }
+                return Sum * Multiplier;
+            }
+
+            public bool IsBoardComplete()
+            {
+                for (int y = 0; y < IsGuessed.GetLength(1); y++)
+                {
+                    bool IsComplete = true;
+
+                    for (int x = 0; x < IsGuessed.GetLength(0); x++)
+                    {
+                        if (IsGuessed[x, y] == false) IsComplete = false;
+                    }
+
+                    if (IsComplete) return true;
+                }
+
+                for (int x = 0; x < IsGuessed.GetLength(0); x++)
+                {
+                    bool IsComplete = true;
+
+                    for (int y = 0; y < IsGuessed.GetLength(1); y++)
+                    {
+                        if (IsGuessed[x, y] == false) IsComplete = false;
+                    }
+
+                    if (IsComplete) return true;
+                }
+
+                return false;
+            }
+        }
+
+        static void Part1()
+        {
+            string[] Lines = System.IO.File.ReadAllLines("Data.txt");
+
+            string[] DrawnStrings = Lines[0].Split(',');
+            List<int> DrawnValues = new List<int>();
+
+            for (int i = 0; i < DrawnStrings.Length; i++)
+            {
+                DrawnValues.Add(Convert.ToInt32(DrawnStrings[i]));
+            }
+
+            List<Board> Boards = new List<Board>();
+
+            int BoardSize = 5;
+
+            for (int i = 2; i < Lines.Length; i+=BoardSize+1)
+            {
+                Board CurrentBoard = new Board(new int[BoardSize, BoardSize], new bool[BoardSize, BoardSize]);
+                for (int y = 0; y < BoardSize; y++)
+                {
+                    string[] Values = Lines[i+y].Split(' ');
+                    int x = 0;
+
+                    for (int k = 0; k < Values.Length; k++)
+                    {
+                        if (Values[k] != "")
+                        {
+                            CurrentBoard.Values[x, y] = Convert.ToInt32(Values[k]);
+                            CurrentBoard.IsGuessed[x, y] = false;
+                            x++;
+                        }
+                    }
+                }
+                Boards.Add(CurrentBoard);
+
+            }
+
+            /* Testing
+            Boards.Add(new Board(
+                new int[,] { {3, 1, 2}, {5, 1 , 3}, {4, 2, 2} },
+                new bool[,] { { false, false, false }, { true, true, true }, { false, false, false } })                
+                ); 
+            Boards.Add(new Board(
+                new int[,] { { 3, 1, 2 }, { 5, 1, 3 }, { 4, 2, 2 } },
+                new bool[,] { { false, false, false }, { false, false, false }, { false, false, false } })
+                );
+            Boards.Add(new Board(
+               new int[,] { { 3, 1, 2 }, { 5, 1, 3 }, { 4, 2, 2 } },
+               new bool[,] { { true, false, false }, { true, false, false }, { true, false, false } })
+               );
+
+            for (int i = 0; i < Boards.Count; i++)
+            {
+                Console.WriteLine(IsBoardComplete(Boards[i]));
+            }
+            */
+
+            int WinningBoardScore = 0;
+
+            for (int i = 0; i < DrawnValues.Count; i++)
+            {
+                for (int j = 0; j < Boards.Count; j++)
+                {
+                    Boards[j].Guess(DrawnValues[i]);
+                    if (Boards[j].IsBoardComplete())
+                    {
+                        WinningBoardScore = Boards[j].Score(DrawnValues[i]);
+                        i = DrawnValues.Count;
+                        j = Boards.Count;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(WinningBoardScore);
+            Console.ReadKey();
+        }
+        
+        static void Part2()
+        {
+            string[] Lines = System.IO.File.ReadAllLines("Data.txt");
+
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
