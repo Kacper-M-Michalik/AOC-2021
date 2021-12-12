f0f50dd87553e75fc4b5fbda3410625b15b7f469
Kamichalik1 <kacper.m.michalik@gmail.com>
2021-12-12 15:06:01 +0000

    Day 12 Part 2

--- AOC2021/Day12/Program.cs
+++ AOC2021/Day12/Program.cs
@@ -40,11 +40,11 @@ namespace Day12
             }
 
             int Paths = 0;
-            MoveToCave("start", new List<string>());
+            MoveToCave("start", new HashSet<string>());
             Console.WriteLine(Paths);
             Console.ReadLine();
 
-            void MoveToCave(string CurrentCave, List<string> VistedCaves)
+            void MoveToCave(string CurrentCave, HashSet<string> VistedCaves)
             {
                 VistedCaves.Add(CurrentCave);
 
@@ -58,14 +58,67 @@ namespace Day12
                 {
                     if (!char.IsLower(CaveConnectionLookup[CurrentCave][i][0]) || (char.IsLower(CaveConnectionLookup[CurrentCave][i][0]) && !VistedCaves.Contains(CaveConnectionLookup[CurrentCave][i])))
                     {
-                        MoveToCave(CaveConnectionLookup[CurrentCave][i], new List<string>(VistedCaves));
+                        MoveToCave(CaveConnectionLookup[CurrentCave][i], new HashSet<string>(VistedCaves));
                     }
                 }
             }
         }
+
         static void Part2()
         {
+            string[] Lines = System.IO.File.ReadAllLines("Data.txt");
+            Dictionary<string, List<string>> CaveConnectionLookup = new Dictionary<string, List<string>>();
 
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                string[] Connection = Lines[i].Split('-');
+
+                if (CaveConnectionLookup.ContainsKey(Connection[0]))
+                {
+                    CaveConnectionLookup[Connection[0]].Add(Connection[1]);
+                }
+                else
+                {
+                    CaveConnectionLookup.Add(Connection[0], new List<string>() { Connection[1] });
+                }
+
+                if (CaveConnectionLookup.ContainsKey(Connection[1]))
+                {
+                    CaveConnectionLookup[Connection[1]].Add(Connection[0]);
+                }
+                else
+                {
+                    CaveConnectionLookup.Add(Connection[1], new List<string>() { Connection[0] });
+                }
+            }
+
+            long Paths = 0;
+            MoveToCave("start", new HashSet<string>(), false);
+            Console.WriteLine(Paths);
+            Console.ReadLine();
+
+            void MoveToCave(string CurrentCave, HashSet<string> VistedCaves, bool HasDoubleVisitedSmallCave)
+            {
+                VistedCaves.Add(CurrentCave);
+
+                if (CurrentCave == "end")
+                {
+                    Paths++;
+                    return;
+                }
+
+                for (int i = 0; i < CaveConnectionLookup[CurrentCave].Count; i++)
+                {
+                    if (!char.IsLower(CaveConnectionLookup[CurrentCave][i][0]) || (char.IsLower(CaveConnectionLookup[CurrentCave][i][0]) && !VistedCaves.Contains(CaveConnectionLookup[CurrentCave][i])))
+                    {
+                        MoveToCave(CaveConnectionLookup[CurrentCave][i], new HashSet<string>(VistedCaves), HasDoubleVisitedSmallCave);
+                    }
+                    else if (!HasDoubleVisitedSmallCave && CaveConnectionLookup[CurrentCave][i] != "start")
+                    {
+                        MoveToCave(CaveConnectionLookup[CurrentCave][i], new HashSet<string>(VistedCaves), true);
+                    }
+                }
+            }
         }
     }
 }
