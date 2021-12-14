10a1122600df25ded5f0d96d3124858981e15f79
Kamichalik1 <kacper.m.michalik@gmail.com>
2021-12-14 16:22:13 +0000

    Day 14 P1&P2 Fixed

--- AOC2021/Day14/Program.cs
+++ AOC2021/Day14/Program.cs
@@ -15,86 +15,9 @@ namespace Day14
         {
             string[] Lines = System.IO.File.ReadAllLines("Data.txt");
 
-            Dictionary<string, int> PairCount = new Dictionary<string, int>();
-            Dictionary<string, List<string>> PairLookup = new Dictionary<string, List<string>>();
-
-            for (int i = 0; i < Lines[0].Length-1; i++)
-            {
-                string Pair = Lines[0][i].ToString() + Lines[0][i+1].ToString();
-                if (PairCount.ContainsKey(Pair))
-                {
-                    PairCount[Pair]++;
-                }
-                else
-                {
-                    PairCount.Add(Pair, 1);
-                }
-            }
-
-            for (int i = 2; i < Lines.Length; i++)
-            {
-                string[] PairData = Lines[i].Split(" -> ");
-                PairLookup.Add(PairData[0], new List<string>() { PairData[0][0].ToString() + PairData[1], PairData[1] + PairData[0][1].ToString()});
-            }
-
-            for (int i = 0; i < 10; i++)
-            {
-                Dictionary<string, int> NewPairCount = new Dictionary<string, int>();
-                foreach (KeyValuePair<string, int> Pair in PairCount)
-                {
-                    List<string> NewPairsList = PairLookup[Pair.Key];
-                    for (int j = 0; j < NewPairsList.Count; j++)
-                    {
-                        if (NewPairCount.ContainsKey(NewPairsList[j]))
-                        {
-                            NewPairCount[NewPairsList[j]] += Pair.Value;
-                        }
-                        else
-                        {
-                            NewPairCount.Add(NewPairsList[j], Pair.Value);
-                        }
-                    }
-                }
-                PairCount = NewPairCount;                
-            }
-
-            int LargestCount = 0;
-            int SmallestCount = int.MaxValue;
-            Dictionary<char, int> CharCount = new Dictionary<char, int>();
-            foreach (KeyValuePair<string, int> Pair in PairCount)
-            {
-                if (CharCount.ContainsKey(Pair.Key[0]))
-                {
-                    CharCount[Pair.Key[0]] += Pair.Value;
-                }
-                else
-                {
-                    CharCount.Add(Pair.Key[0], Pair.Value);
-                }
-                if (CharCount.ContainsKey(Pair.Key[1]))
-                {
-                    CharCount[Pair.Key[1]] += Pair.Value;
-                }
-                else
-                {
-                    CharCount.Add(Pair.Key[1], Pair.Value);
-                }
-            }
-
-            foreach (KeyValuePair<char, int> Pair in CharCount)
-            {
-                if (Pair.Value > LargestCount) LargestCount = Pair.Value;
-                if (Pair.Value < SmallestCount) SmallestCount = Pair.Value;
-            }
-            //Answer within range +-1
-            Console.Write(Math.Ceiling((LargestCount-SmallestCount)*0.5f));
-        }
-        static void Part2()
-        {
-            string[] Lines = System.IO.File.ReadAllLines("Data.txt");
-
             Dictionary<string, long> PairCount = new Dictionary<string, long>();
             Dictionary<string, List<string>> PairLookup = new Dictionary<string, List<string>>();
+            Dictionary<char, long> CharacterDuplicates = new Dictionary<char, long>();
 
             for (int i = 0; i < Lines[0].Length - 1; i++)
             {
@@ -107,7 +30,20 @@ namespace Day14
                 {
                     PairCount.Add(Pair, 1);
                 }
+
+                if (i != 0)
+                {
+                    if (CharacterDuplicates.ContainsKey(Lines[0][i]))
+                    {
+                        CharacterDuplicates[Lines[0][i]]++;
+                    }
+                    else
+                    {
+                        CharacterDuplicates.Add(Lines[0][i], 1);
+                    }
+                }
             }
+            if (!CharacterDuplicates.ContainsKey(Lines[0][Lines[0].Length - 1])) CharacterDuplicates.Add(Lines[0][Lines[0].Length - 1], 0);
 
             for (int i = 2; i < Lines.Length; i++)
             {
@@ -115,7 +51,7 @@ namespace Day14
                 PairLookup.Add(PairData[0], new List<string>() { PairData[0][0].ToString() + PairData[1], PairData[1] + PairData[0][1].ToString() });
             }
 
-            for (int i = 0; i < 40; i++)
+            for (int i = 0; i < 10; i++)
             {
                 Dictionary<string, long> NewPairCount = new Dictionary<string, long>();
                 foreach (KeyValuePair<string, long> Pair in PairCount)
@@ -131,6 +67,14 @@ namespace Day14
                         {
                             NewPairCount.Add(NewPairsList[j], Pair.Value);
                         }
+                    }
+                    if (CharacterDuplicates.ContainsKey(NewPairsList[0][1]))
+                    {
+                        CharacterDuplicates[NewPairsList[0][1]] += Pair.Value;
+                    }
+                    else
+                    {
+                        CharacterDuplicates.Add(NewPairsList[0][1], Pair.Value);
                     }
                 }
                 PairCount = NewPairCount;
@@ -161,11 +105,116 @@ namespace Day14
 
             foreach (KeyValuePair<char, long> Pair in CharCount)
             {
-                if (Pair.Value > LargestCount) LargestCount = Pair.Value;
-                if (Pair.Value < SmallestCount) SmallestCount = Pair.Value;
+                long TrueCount = Pair.Value - CharacterDuplicates[Pair.Key];
+
+                if (TrueCount > LargestCount) LargestCount = TrueCount;
+                if (TrueCount < SmallestCount) SmallestCount = TrueCount;
             }
-            //Answer within range +-1
-            Console.Write(Math.Ceiling((LargestCount - SmallestCount) * 0.5f));
+
+            Console.WriteLine(LargestCount - SmallestCount);
+        }
+
+        static void Part2()
+        {
+            string[] Lines = System.IO.File.ReadAllLines("Data.txt");
+
+            Dictionary<string, long> PairCount = new Dictionary<string, long>();
+            Dictionary<string, List<string>> PairLookup = new Dictionary<string, List<string>>();
+            Dictionary<char, long> CharacterDuplicates = new Dictionary<char, long>();
+
+            for (int i = 0; i < Lines[0].Length - 1; i++)
+            {
+                string Pair = Lines[0][i].ToString() + Lines[0][i + 1].ToString();
+                if (PairCount.ContainsKey(Pair))
+                {
+                    PairCount[Pair]++;
+                }
+                else
+                {
+                    PairCount.Add(Pair, 1);
+                }
+
+                if (i != 0)
+                {
+                    if (CharacterDuplicates.ContainsKey(Lines[0][i]))
+                    {
+                        CharacterDuplicates[Lines[0][i]]++;
+                    }
+                    else
+                    {
+                        CharacterDuplicates.Add(Lines[0][i], 1);
+                    }
+                }
+            }
+            if (!CharacterDuplicates.ContainsKey(Lines[0][Lines[0].Length - 1])) CharacterDuplicates.Add(Lines[0][Lines[0].Length - 1], 0);
+
+            for (int i = 2; i < Lines.Length; i++)
+            {
+                string[] PairData = Lines[i].Split(" -> ");
+                PairLookup.Add(PairData[0], new List<string>() { PairData[0][0].ToString() + PairData[1], PairData[1] + PairData[0][1].ToString() });
+            }
+
+            for (int i = 0; i < 40; i++)
+            {
+                Dictionary<string, long> NewPairCount = new Dictionary<string, long>();
+                foreach (KeyValuePair<string, long> Pair in PairCount)
+                {
+                    List<string> NewPairsList = PairLookup[Pair.Key];
+                    for (int j = 0; j < NewPairsList.Count; j++)
+                    {
+                        if (NewPairCount.ContainsKey(NewPairsList[j]))
+                        {
+                            NewPairCount[NewPairsList[j]] += Pair.Value;
+                        }
+                        else
+                        {
+                            NewPairCount.Add(NewPairsList[j], Pair.Value);
+                        }
+                    }
+                    if (CharacterDuplicates.ContainsKey(NewPairsList[0][1]))
+                    {
+                        CharacterDuplicates[NewPairsList[0][1]] += Pair.Value;
+                    }
+                    else
+                    {
+                        CharacterDuplicates.Add(NewPairsList[0][1], Pair.Value);
+                    }
+                }
+                PairCount = NewPairCount;
+            }
+
+            long LargestCount = 0;
+            long SmallestCount = long.MaxValue;
+            Dictionary<char, long> CharCount = new Dictionary<char, long>();
+            foreach (KeyValuePair<string, long> Pair in PairCount)
+            {
+                if (CharCount.ContainsKey(Pair.Key[0]))
+                {
+                    CharCount[Pair.Key[0]] += Pair.Value;
+                }
+                else
+                {
+                    CharCount.Add(Pair.Key[0], Pair.Value);
+                }
+                if (CharCount.ContainsKey(Pair.Key[1]))
+                {
+                    CharCount[Pair.Key[1]] += Pair.Value;
+                }
+                else
+                {
+                    CharCount.Add(Pair.Key[1], Pair.Value);
+                }
+            }
+
+            foreach (KeyValuePair<char, long> Pair in CharCount)
+            {
+                long TrueCount = Pair.Value - CharacterDuplicates[Pair.Key];
+
+                if (TrueCount > LargestCount) LargestCount = TrueCount;
+                if (TrueCount < SmallestCount) SmallestCount = TrueCount;
+            }
+
+            Console.WriteLine(LargestCount - SmallestCount);
         }
     }
 }
